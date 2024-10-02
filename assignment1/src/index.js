8597aea40a3922de054da1fed056d2022339d249
KamKam <gavincheng524@gmail.com>
2024-10-02 11:38:52 -0400

    Final1

---
+++ assignment1/src/index.js
@@ -0,0 +1,20 @@
+/*
+Student Name: Tak Kam Cheng
+Student Id: 301429108
+Date: 26 Sep 2024
+File: index.js
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
